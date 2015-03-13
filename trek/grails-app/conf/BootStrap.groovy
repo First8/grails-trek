a594126473c8b8e53f5bad0ddcb7025316a493de
bramvonk <b.vonk@first8.nl>
2015-03-13 13:32:14 +0100

    Show users in console, for easier access

--- trek/grails-app/conf/BootStrap.groovy
+++ trek/grails-app/conf/BootStrap.groovy
@@ -20,7 +20,11 @@ class BootStrap {
 		assert Person.count() > 0
 		
 		// create a few users to log in with
-		new User(userName: "jeanluc", password: "123", person: Person.findByName("Jean-Luc Picard")).save()
-		new User(userName: "worf", password: "123", person: Person.findByName("Worf")).save()
+		def users = [] //new list
+		users << new User(userName: "jeanluc", password: "123", person: Person.findByName("Jean-Luc Picard")).save() //"The save method returns (...) the instance itself if successful."
+		users << new User(userName: "worf", password: "123", person: Person.findByName("Worf")).save()
+		users.each { 
+			log.info("There is a user in the database with user name ${it.userName} and password ${it.password}")
+		}
 	}
 }
