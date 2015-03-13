6cd9369e1134f0c28d285824e6fe6b7323a14c2c
bramvonk <b.vonk@first8.nl>
2015-03-13 16:45:49 +0100

    Add assignment

--- trek/grails-app/domain/nl/first8/trek/Person.groovy
+++ trek/grails-app/domain/nl/first8/trek/Person.groovy
@@ -1,6 +1,13 @@
 package nl.first8.trek
 
 class Person {
+	//TODON  take a look in the dbconsole (see Admin menu in your browser) to see how domain classes end up in the database.
+	//       In the back-end, GORM (Grails Object Relational Mapping), http://grails.github.io/grails-doc/2.4.4/guide/GORM.html,
+	//       uses hibernate.
+	//       Questions to ask yourself:
+	//       - where did the id field come from? Hint: convention over configuration.
+	//       - what happened to the Gender?
+	
 	
 	enum Gender {
 		M, F
