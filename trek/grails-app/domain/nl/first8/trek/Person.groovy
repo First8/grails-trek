d2bde626ac41b610805fd67aa9aa8819612c7de4
bramvonk <b.vonk@first8.nl>
2015-03-20 12:04:11 +0100

    Slightly better comment

--- trek/grails-app/domain/nl/first8/trek/Person.groovy
+++ trek/grails-app/domain/nl/first8/trek/Person.groovy
@@ -5,13 +5,14 @@ import nl.first8.trek.security.User
 class Person {
 	/*
 	  <pre>
-	TODON  take a look in the dbconsole (see Admin menu in your browser) to see how domain classes end up in the database.
+	TODON  take a look in the dbconsole (see Admin menu in the web application) to see how domain classes end up in the database.
 	       Unfortunately, you might need to change the JDBC URL to jdbc:h2:mem:devDb yourself.
 	
 	       In the back-end, <a href='http://grails.github.io/grails-doc/2.4.4/guide/GORM.html'>GORM (Grails Object Relational Mapping</a>,
 	       uses hibernate.
 	       Questions to ask yourself:
 	       - where did the id field come from? Hint: convention over configuration.
+	       - what happened with the names of the fields?
 	       - what happened to the Gender?
 		</pre>
 	*/
