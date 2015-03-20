093464b90df180ea783adfbbd019144cc7360754
bramvonk <b.vonk@first8.nl>
2015-03-20 09:11:03 +0100

    HTML-ify comment

--- trek/grails-app/domain/nl/first8/trek/Person.groovy
+++ trek/grails-app/domain/nl/first8/trek/Person.groovy
@@ -1,13 +1,20 @@
 package nl.first8.trek
 
+import nl.first8.trek.security.User
+
 class Person {
-	//TODON  take a look in the dbconsole (see Admin menu in your browser) to see how domain classes end up in the database.
-	//       In the back-end, GORM (Grails Object Relational Mapping), http://grails.github.io/grails-doc/2.4.4/guide/GORM.html,
-	//       uses hibernate.
-	//       Questions to ask yourself:
-	//       - where did the id field come from? Hint: convention over configuration.
-	//       - what happened to the Gender?
+	/*
+	  <pre>
+	TODON  take a look in the dbconsole (see Admin menu in your browser) to see how domain classes end up in the database.
+	       Unfortunately, you might need to change the JDBC URL to jdbc:h2:mem:devDb yourself.
 	
+	       In the back-end, <a href='http://grails.github.io/grails-doc/2.4.4/guide/GORM.html'>GORM (Grails Object Relational Mapping</a>,
+	       uses hibernate.
+	       Questions to ask yourself:
+	       - where did the id field come from? Hint: convention over configuration.
+	       - what happened to the Gender?
+		</pre>
+	*/
 	
 	enum Gender {
 		M, F
@@ -36,4 +43,12 @@ class Person {
     }
 	
 	String toString() { name }
+	
+	def beforeDelete () {
+		User.withNewSession {
+		  User.findAllByPerson(this).each {User user ->
+			user.delete(flush: true)
+		  }
+		}
+	}
 }
