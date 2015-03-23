990034cc5f3bdc76feab44148c67260a3d9558d1
bwijsmuller <github@wijsmullerbros.nl>
2015-03-23 11:44:53 +0100

    better todo explanation

--- trek/grails-app/controllers/nl/first8/trek/MissionController.groovy
+++ trek/grails-app/controllers/nl/first8/trek/MissionController.groovy
@@ -6,7 +6,7 @@ import grails.converters.JSON
  * No view here yet. The data is directly 'render'-ed to the output in JSON format.
  * In standard Spring MVC that Grails uses, a model (object) would be returned to the view. 
  *
- * TODON: Create a view that display the plots data. Note that the current JSON format
+ * TODON: Create a view that displays the various plots. Note that the current JSON format
  *  may not be the most suitable structure to select from in a view. 
  */
 class MissionController {
