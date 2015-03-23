e3a25a4e768c1b7c3efcb8ccb8efa4236c30a2b8
bwijsmuller <github@wijsmullerbros.nl>
2015-03-23 13:55:12 +0100

    typos fixed

--- trek/grails-app/controllers/nl/first8/trek/SpaceshipClassController.groovy
+++ trek/grails-app/controllers/nl/first8/trek/SpaceshipClassController.groovy
@@ -1,6 +1,7 @@
 package nl.first8.trek
 
 class SpaceshipClassController {
+	
     static scaffold = true
 	
 	/**
@@ -8,8 +9,8 @@ class SpaceshipClassController {
 	 * Problem: Whenever a save fails (for instance, the name is not unique) the organization is still saved.
 	 * 
 	 * Since controller methods are not automatically transactional, we would like to handle
-	 * the action in a more transactional way, in a service method. Create a service method an
-	 * use it here to either have to commits successful or have both fail together.
+	 * the action in a more transactional way, in a service method. Create a service method and
+	 * use it here to either have the two changes successful or have both fail together.
 	 */
 	def save(SpaceshipClass instance) {
 		log.info "Creating new space ship class ${instance?.name}"
