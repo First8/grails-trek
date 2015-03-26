efb111415e4c51ba39585940021f68d9503f41c9
bwijsmuller <github@wijsmullerbros.nl>
2015-03-26 11:33:05 +0100

    fixed bug for windows distro plus added trek project correctly

--- ggts-multiplatform/create-distros.sh
+++ ggts-multiplatform/create-distros.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-mkdir -p ./target
+DIR=`pwd`
+PROJECT="trek"
 
-echo "linux ..."
-rm -f ./target/trek-linux.zip
-zip -1uqry ./target/trek-linux.zip changedFiles ggts-bundle-linux-x64 prepare.sh workspace ../trek
+mkdir -p "${DIR}/target"
 
 echo "windows ..."
-rm -f ./target/trek-windows.zip
-zip -1uqry ./target/trek-windows.zip changedFiles ggts-bundle-win32-i586 sed-win32 prepare.bat workspace ../trek
+#rm -f "${DIR}/target/trek-windows.zip" && \
+#zip -1uqry "$DIR/target/trek-windows.zip" changedFiles ggts-bundle-win32-i586 prepare.bat workspace resources/sed-win32
+
+echo "linux ..."
+#rm -f "${DIR}/target/trek-linux.zip"
+#zip -1uqry "${DIR}/target/trek-linux.zip" changedFiles ggts-bundle-linux-x64 prepare.sh workspace
 
 echo "mac ..."
-rm -f ./target/trek-mac.zip
-zip -1uqry ./target/trek-mac.zip changedFiles ggts-bundle-osx-x64 prepare.sh workspace ../trek
+#rm -f "${DIR}/target/trek-mac.zip" && \
+#zip -1uqry "${DIR}/target/trek-mac.zip" changedFiles ggts-bundle-osx-x64 prepare.sh workspace
+
+echo "adding project ..." 
+pushd ./..
+   zip -rq "${DIR}/target/trek-windows.zip" ${PROJECT}
+   zip -rq "${DIR}/target/trek-linux.zip" ${PROJECT}
+   zip -rq "${DIR}/target/trek-mac.zip" ${PROJECT}
+popd
